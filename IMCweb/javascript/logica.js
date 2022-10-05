002d24fab4eced00af655cb1cda112f357675b3a
lucas pedroso <99222272+1LUCASPEDROSO@users.noreply.github.com>
2022-10-05 12:09:05 -0300

    subindo verão "completa" de imc
    adcionado funtion de calcular imc com if e else if, criado function limpar dados para deoxar valores de input html zerados

--- IMCweb/javascript/logica.js
+++ IMCweb/javascript/logica.js
@@ -1,9 +1,35 @@
 
-function resultado(imc){
-
+function resultado(){
     var peso = parseFloat(document.querySelector('#peso').value.replace(',','.'));
     var altura = parseFloat(document.querySelector('#altura').value.replace(',','.'));
-    var imc = peso / (altura * altura)
+    var resultadoimc = peso / (altura * altura);
+    var imagemresultado = document.createElement("img");
 
-    alert(imc);
+    //como injetar css atráves do js
+    imagemresultado.style.cssText="margin-left:42%"
+    
+    if(resultadoimc < 18.5){
+        alert("imc: "+resultadoimc)
+        imagemresultado.src="../imagem/homemmagro.jpg";
+       document.querySelector("#imagem").appendChild(imagemresultado)
+       Limpardados()
+    }
+    else if(resultadoimc > 18.6 && resultadoimc < 24.9){
+        alert("imc: "+resultadoimc)
+        imagemresultado.src="../imagem/homempesoideal.jpg";
+        document.querySelector("#imagem").appendChild(imagemresultado)
+        Limpardados()
+    }
+    else if(resultadoimc > 25 && resultadoimc < 29.9){
+        alert("imc: "+resultadoimc)
+        imagemresultado.src="../imagem/homemgordo.jpg";
+        document.querySelector("#imagem").appendChild(imagemresultado)
+        Limpardados()
+    }
+}
+function Limpardados()
+{
+    document.querySelector("#peso").value=0
+    document.querySelector("#altura").value=0
+ 
 }
