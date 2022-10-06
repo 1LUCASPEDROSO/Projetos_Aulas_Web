efd7c6e469c54d18b39478eec1e289a91c0d6b4b
lucas pedroso <99222272+1LUCASPEDROSO@users.noreply.github.com>
2022-10-06 12:20:51 -0300

    subindo versão 1

---
+++ ProjetoMesada/Js/main.js
@@ -0,0 +1,8 @@
+
+const valorInicial = 100;
+
+function CaluclarMesada(){
+    var ocorrencias = parseInt(document.querySelector("#oc").value)
+    var total = valorInicial - ocorrencias;
+    console.log(total)
+}
