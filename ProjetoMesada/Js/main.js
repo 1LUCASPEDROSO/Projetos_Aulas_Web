aae8d80850b41f6e7bfc63126c95de633f029624
lucas pedroso <99222272+1LUCASPEDROSO@users.noreply.github.com>
2022-10-08 21:40:50 -0300

    Subindo versão 1.0.1
    adcionado inputs para calculo do valor de oceorrencia junto com lógica de validação de valor no main.js

--- ProjetoMesada/Js/main.js
+++ ProjetoMesada/Js/main.js
@@ -1,8 +1,66 @@
-
-const valorInicial = 100;
-
-function CaluclarMesada(){
-    var ocorrencias = parseInt(document.querySelector("#oc").value)
-    var total = valorInicial - ocorrencias;
-    console.log(total)
+var ValorOcorrencia = [0.25,0.50,0.75,1.00]
+var TotalOcorrencia
+var TotalFinal
+const ValorMensal = 100
+//the name of functions is basead in positon in array valorOcorrencia
+function CaluclarMesada0(){
+    var CountOcorrencia = parseInt(document.querySelector('#input0').value)
+    if(CountOcorrencia < 0){
+       alert("Valores negativos não são aceitos")
+        document.querySelector('#input0'.value=0)
+       do{
+           var CountOcorrencia = parseInt(document.querySelector('#input0').value)
+      }while(CountOcorrencia <=0);
+    }
+    {
+        TotalOcorrencia = CountOcorrencia * ValorOcorrencia[0]
+        TotalFinal = ValorMensal - TotalOcorrencia
+        document.querySelector("#total0").value=TotalOcorrencia
+    }
 }
+function CaluclarMesada1(){
+    var CountOcorrencia = parseInt(document.querySelector('#input1').value)
+    if(CountOcorrencia < 0){
+        alert("Valores negativos não são aceitos")
+         document.querySelector('#input1'.value=0)
+        do{
+            var CountOcorrencia = parseInt(document.querySelector('#input1').value)
+       }while(CountOcorrencia <=0);
+     }
+     {
+         TotalOcorrencia = CountOcorrencia * ValorOcorrencia[1]
+         TotalFinal = ValorMensal - TotalOcorrencia
+         document.querySelector("#total1").value=TotalOcorrencia
+     }
+}
+function CaluclarMesada2(){
+    var CountOcorrencia = parseInt(document.querySelector('#input2').value)
+    if(CountOcorrencia < 0){
+        alert("Valores negativos não são aceitos")
+         document.querySelector('#input2'.value=0)
+        do{
+            var CountOcorrencia = parseInt(document.querySelector('#input2').value)
+       }while(CountOcorrencia <=0);
+     }
+     {
+         TotalOcorrencia = CountOcorrencia * ValorOcorrencia[2]
+         TotalFinal = ValorMensal - TotalOcorrencia
+         document.querySelector("#total2").value=TotalOcorrencia
+     }
+}
+function CaluclarMesada3(){
+    var CountOcorrencia = parseInt(document.querySelector('#input3').value)
+    if(CountOcorrencia < 0){
+        alert("Valores negativos não são aceitos")
+         document.querySelector('#input3'.value=0)
+        do{
+            var CountOcorrencia = parseInt(document.querySelector('#input3').value)
+       }while(CountOcorrencia <=0);
+     }
+     {
+         TotalOcorrencia = CountOcorrencia * ValorOcorrencia[3]
+         TotalFinal = ValorMensal - TotalOcorrencia
+         document.querySelector("#total3").value=TotalOcorrencia
+     }
+}
+
