792c0004522410672b88ac548b1e6df16b52cc5d
lucas pedroso <99222272+1LUCASPEDROSO@users.noreply.github.com>
2022-10-24 19:50:06 -0300

    versão 2

--- projetoSalaDecinema/js/main.js
+++ projetoSalaDecinema/js/main.js
@@ -10,11 +10,24 @@ dadoscinema(cod,nom,dt,hr,prc,tp,sl) {
      this.preco = prc;
      this.tipo = tp;
      this.sala = sl;
-     ingresso[ingresso.lenght] = new this.dadoscinema(cod,nom,dt,hr,prc,tp,sl);
+ }
+ adcionaringresso(cod,nom,dt,hr,prc,tp,sl){
+    ingresso[ingresso.lenght] = new this.dadoscinema(cod,nom,dt,hr,prc,tp,sl);
+    for (let i = 0; i < ingresso.length; i++) {
+        console.log(ingresso[i].codigo)
+        console.log(ingresso[i].nomefilme)
+        console.log(ingresso[i].data)
+        console.log(ingresso[i].hora)
+        console.log(ingresso[i].preco)
+        console.log(ingresso[i].tipo)
+        console.log(ingresso[i].salas)
+        
+     }
  }
 
-
-
+ mostraringressos(){
+ 
+ }
 
 
 
