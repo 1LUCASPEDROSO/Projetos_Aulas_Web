41b41ae7974d099552cd045b3ee6ab681231dda8
lucas pedroso <pedrosolucas1745@gmail.com>
2022-11-11 12:00:13 -0300

    subindo versão 4

--- SistemadeComprasCompleto/php/Dadosuser.php
+++ SistemadeComprasCompleto/php/Dadosuser.php
@@ -1,11 +1,32 @@
-<?php 
-
-
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="css/estilos.css">
+</head>
+<body>
+ 
+    <div class='ContainerMain'>
+        <div class='ContainerLogin'>
+           <img  id='imagemLogin' src='imagens/imagemLogin.png' alt='Login'>
+            <form id='Formulariologin' action='php/Dadosuser.php' method='post'>
+                <label id='formalabels' for='Email'>Email:</label>
+                <input type='Email' id='Email'>
+                <label id='formalabels' for='Senha'>Senha:</label>
+                <input type='password' id='Senha'>
+                <button id='btnLogin' action='validaçãoLogin()'>Login</button>
+            </form>
+        </div>
+    </div>
+    <?php 
 function validaçãoLogin()
 {
     $email1 = $_POST["Email"];
     $senha1 = $_POST["Senha"];
-    if($email1 != "pedrosolucas1745@gmail.com" || $senha1="viper1")
+    if($email1 != "pedrosolucas1745@gmail.com" || $senha1!="viper1")
     {
         header('Location: TelaCadastro.html');
         $emailcadastro= $_POST["EmailCadastro"];
@@ -15,7 +36,7 @@ function validaçãoLogin()
             header('Location: TelaProdutos.html');
         }
         elseif($emailcadastro || $senhacadastro = null){
-            header("location: TelaError404.php");
+            header("Location: TelaError404.php");
         }
     }
     else{
@@ -23,3 +44,7 @@ function validaçãoLogin()
     }
 }
 ?>
+</body> 
+</html>
+<script src="js/login.js"></script>
+<script src="js/main.js"></script>
---
+++ exercicio01/compra.php
@@ -0,0 +1,112 @@
+﻿<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Compra</title>
+
+<style>
+	table
+	{
+		padding:10%;
+		margin-left:33%;
+		background-color:#FFF;	
+	}
+	#topo
+	{
+		
+		width:30%;
+		margin-left:35%;
+			
+	}
+	#fundo
+	{
+		background-image:url(imagem/fundo2.jpg);
+		background-repeat:repeat;	
+	}
+	caption
+	{
+		font-size:30px;
+		color:#FFF;	
+	}
+	
+</style>
+
+
+
+</head>
+
+<body id="fundo">
+
+
+
+<?php
+
+$nome= $_POST["nome2"];
+$cpf = $_POST["cpf2"];
+$escolha = $_POST["opcao"];
+//echo $nome."<br>".$cpf."<br>";
+$pagamento;
+$cont=0;
+
+$produtos= $_REQUEST['produto'];
+
+if (!empty($produtos)) {                
+      $qtd = count($produtos);
+       for ($i = 0; $i < $qtd; $i++) {
+          // echo $produtos[$i]."<br>";//imprime os itens 
+		   $cont = $cont + $produtos[$i];
+       }
+//	   echo "Total:  ".$cont;
+ }
+ 
+ switch( isset( $escolha ) )
+ {
+ 	case 'dinheiro':
+		$pagamento=$cont - ($cont*0.05);
+	break;
+	case '5x':
+		$pagamento=$cont + ($cont*0.05);
+	break;
+	case '8x':
+		$pagamento=$cont + ($cont*0.1);
+	break;
+	case '12x':
+		$pagamento=$cont + ($cont*0.15);
+	break;
+	
+ }
+ //echo "<br>pagamento:  ".$pagamento;
+ 
+ 
+ 
+
+?>
+
+<div >
+	<img id="topo" src="imagem/logo2.jpg">
+</div>
+
+<div>
+<table>
+  <caption>
+    <u>Comprovante de compra:</u>
+  </caption>
+  <tr>
+    <td>NOME:</td>
+    <td><?php echo $nome; ?></td>
+  </tr>
+  <tr>
+    <td>CPF:</td>
+    <td><?php echo $cpf; ?></td>
+  </tr>
+  <tr>
+    <td>TOTAL A PAGAR:</td>
+    <td><?php echo "R$ ".number_format($pagamento, 2, ','); ?></td>
+  </tr>
+</table>
+</div>
+
+
+
+</body>
+</html>
