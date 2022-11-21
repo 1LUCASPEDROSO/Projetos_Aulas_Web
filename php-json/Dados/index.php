464824df719d83fb0b0acff3764b94d33fe514a9
lucas pedroso <pedrosolucas1745@gmail.com>
2022-11-21 12:01:15 -0300

    Subindo primeira verão de Php com Json
    Sistema para alimentar json com dados e passar para .Svg, dados digitados

---
+++ php-json/Dados/index.php
@@ -0,0 +1,25 @@
+<?php
+$jsondata = '[
+    {"nome": "lucas","idade":"20","UC":"Desenvolver-backend"},
+    {"nome": "thiago","idade":"32","UC":"Desenvolver-backend"},
+    {"nome": "1drakoon1","idade":"20","UC":"Desenvolver-backend"},
+    {"nome": "gabe-monta","idade":"19","UC":"Desenvolver-backend"},
+    {"nome": "Gilmar","idade":"46","UC":"Desenvolver-backend"}
+]';
+// criando variavel para decodificar Json e converter dados para um array
+$JsonDescodificando = json_decode($jsondata,true);
+
+// criando arquivo CSV
+$csv = 'alunos.csv';
+
+// criando variavel ponteiro para apontar posições no Json
+$file_pointer = fopen($csv,'w');
+
+// unir array json com um arquivo
+foreach($JsonDescodificando as $i)
+{
+    fputcsv($file_pointer, $i);
+}
+// fechando ponteiro 
+    fclose($file_pointer);
+?>
